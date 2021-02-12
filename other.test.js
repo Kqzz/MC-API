e961abc260e07eaece53e1ef8e6bbf8890b6818b
Kqzz <48300938+Kqzz@users.noreply.github.com>
2021-02-12 18:40:17 -0500

    update of cape test

--- other.test.js
+++ other.test.js
@@ -1,5 +1,5 @@
 const { OptifineCape } = require("./other");
 
 test("checks if mc account has optifine cape", async () => {
-  expect(await OptifineCape('notch')).toEqual(true);
+  expect(await OptifineCape('notch')).toEqual({ has_cape: true, cape_url: 'https://s.optifine.net/capes/notch.png' });
 });
