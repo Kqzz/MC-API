5afb85b8c134a901b111616d8fb5f30193c8dd2e
Kqzz <kqzz@kqzz>
2021-02-16 11:59:20 -0500

    add more descriptive status messages

--- namemcApi.js
+++ namemcApi.js
@@ -11,7 +11,12 @@ router.get("/user/:identifier", async (req, res) => {
 
 router.get("/droptime/:username", async (req, res) => {
     data = await namemc.droptime(req.params.username);
-    res.status(200).send(data)
+    let resp_status = 200;
+    if (data.error != undefined) {
+        resp_status = 400;
+    }
+
+    res.status(resp_status).send(data)
 })
 
 module.exports = router;
