0c5f75bf67470739652f83e63e7f78da53e0fa80
Kqzz <48300938+Kqzz@users.noreply.github.com>
2021-02-22 12:11:59 -0500

    fix /namemc/user/<identifier>

--- routes/namemcApi.js
+++ routes/namemcApi.js
@@ -4,13 +4,24 @@ const router = express.Router();
 const namemc = require('../utils/namemc');
 
 router.get('/user/:identifier', async (req, res) => {
-  const data = await namemc.userStats(req.params.identifier);
-
-  res.send(data);
+  try {
+    res.status(200).send(await namemc.userStats(req.params.identifier));
+  }
+  catch (err) {
+    res.status(err.status).send(err);
+  }
 });
 
 router.get('/droptime/:username', async (req, res) => {
-  const data = await namemc.droptime(req.params.username);
+  let data = {};
+  try {
+    data = await namemc.droptime(req.params.username);
+  }
+  catch (err) {
+    res.status(err.status).send(err);
+    return;
+  }
+
   let resp_status = 200;
   if (data.error !== undefined) {
     resp_status = 400;
