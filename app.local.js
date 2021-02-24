11c8b9fe1827fa42ef8098d5ad02cdcb202e68e5
Kqzz <48300938+Kqzz@users.noreply.github.com>
2021-02-24 15:31:09 -0500

    reformat files

--- app.local.js
+++ app.local.js
@@ -1,5 +1,4 @@
-"use strict";
-const app = require("./app");
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is listening on port ${port}.`));
--- lambda.js
+++ lambda.js
@@ -1,6 +1,5 @@
 const serverless = require('serverless-http');
 const server = require('./app');
+
 const handler = serverless(server);
-module.exports.server = async (event, context) => {
- return await handler(event, context);
-};
+module.exports.server = async (event, context) => await handler(event, context);
