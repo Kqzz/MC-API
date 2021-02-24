2b1b45965ebe40b8320ff27b6c09c8985224f039
Kqzz <48300938+Kqzz@users.noreply.github.com>
2021-02-24 10:41:20 -0500

    add redirect

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.set('json spaces', 0);
 app.use('/api/mojang', mojangAPI); // mojang API routes
 app.use('/api/namemc', namemcAPI); // Namemc API routes
 
+app.get('/', (req, res) => {
+  res.redirect('https://kqzz.github.io/MC-API/#/');
+});
+
 module.exports = app;
