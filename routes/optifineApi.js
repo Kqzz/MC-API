1e0819379ec115cc4c214cae53ec27b3294f8b50
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-03-21 12:31:30 -0400

    add cape cropping endpoint

--- routes/optifineApi.js
+++ routes/optifineApi.js
@@ -8,4 +8,14 @@ router.get('/user/cape/:username', async (req, res) => {
   res.status(200).send(data);
 });
 
+router.get('/user/croppedcape/:username', async (req, res) => {
+  optifine.CroppedOptifineCape(req.params.username, parseInt(req.query.scale, 10) || 1)
+    .then((img) => {
+      res.set({ 'Content-Type': 'image/png' }).status(200).send(img);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
 module.exports = router;
--- utils/optifine.js
+++ utils/optifine.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const Jimp = require('jimp');
 
 exports.OptifineCape = (username) => new Promise((resolve, reject) => {
   const url = `http://s.optifine.net/capes/${username}.png`;
@@ -20,4 +21,27 @@ exports.OptifineCape = (username) => new Promise((resolve, reject) => {
         reject({ error: err, status: 500 });
       }
     });
+});
+
+exports.CroppedOptifineCape = (username, scale) => new Promise((resolve, reject) => {
+  const url = `http://s.optifine.net/capes/${username}.png`;
+  if (scale > 100) {
+    reject({ error: 'use a lower scale LOL', status: 400 });
+  }
+  Jimp.read(url)
+    .then((cape) => {
+      try {
+        resolve(cape
+          .crop(2, 2, 20, 32)
+          .scale(scale, Jimp.RESIZE_NEAREST_NEIGHBOR)
+          .getBufferAsync('image/png'));
+      }
+      catch (err) {
+        resolve(cape
+          .crop(1, 1, 10, 16)
+          .scale(scale, Jimp.RESIZE_NEAREST_NEIGHBOR)
+          .getBufferAsync('image/png'));
+      }
+    })
+    .catch((err) => reject({ error: err, status: 500 }));
 });
