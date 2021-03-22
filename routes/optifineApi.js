23413122d3bf6103f2fa9ad87875b2933bebb0be
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-03-22 09:15:02 -0400

    change optifine routes

--- routes/optifineApi.js
+++ routes/optifineApi.js
@@ -3,15 +3,16 @@ const express = require('express');
 const router = express.Router();
 const optifine = require('../utils/optifine');
 
-router.get('/user/cape/:username', async (req, res) => {
+router.get('/cape/:username', async (req, res) => {
   const data = await optifine.OptifineCape(req.params.username);
   res.status(200).send(data);
 });
 
-router.get('/user/croppedcape/:username', async (req, res) => {
-  optifine.CroppedOptifineCape(req.params.username, parseInt(req.query.scale, 10) || 1)
+router.get('/croppedcape/:username', async (req, res) => {
+  optifine.CroppedOptifineCape(req.params.username, Number(req.query.scale) || 1)
     .then((img) => {
-      res.set({ 'Content-Type': 'image/png' }).status(200).send(img);
+      console.log(Buffer.from(img));
+      res.set({ 'Content-Type': 'image/png' }).status(200).send(Buffer.from(img));
     })
     .catch((err) => {
       res.status(500).send(err);
