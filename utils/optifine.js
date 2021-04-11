cefb38798d45b2c6ba0766d42e2308aa05a69fa0
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-04-11 08:36:38 -0400

    fix scuffed optifine code that doesn't work

--- utils/optifine.js
+++ utils/optifine.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const Jimp = require('jimp');
 
 exports.OptifineCape = (username) => new Promise((resolve, reject) => {
   const url = `http://s.optifine.net/capes/${username}.png`;
@@ -23,25 +22,25 @@ exports.OptifineCape = (username) => new Promise((resolve, reject) => {
     });
 });
 
-exports.CroppedOptifineCape = (username, scale) => new Promise((resolve, reject) => {
-  const url = `http://s.optifine.net/capes/${username}.png`;
-  if (scale > 100) {
-    reject({ error: 'use a lower scale LOL', status: 400 });
-  }
-  Jimp.read(url)
-    .then((cape) => {
-      try {
-        resolve(cape
-          .crop(2, 2, 20, 32)
-          .scale(scale, Jimp.RESIZE_NEAREST_NEIGHBOR)
-          .getBufferAsync('image/png'));
-      }
-      catch (err) {
-        resolve(cape
-          .crop(1, 1, 10, 16)
-          .scale(scale, Jimp.RESIZE_NEAREST_NEIGHBOR)
-          .getBufferAsync('image/png'));
-      }
-    })
-    .catch((err) => reject({ error: err, status: 500 }));
-});
+// exports.CroppedOptifineCape = (username, scale) => new Promise((resolve, reject) => {
+//   const url = `http://s.optifine.net/capes/${username}.png`;
+//   if (scale > 100) {
+//     reject({ error: 'use a lower scale LOL', status: 400 });
+//   }
+//   Jimp.read(url)
+//     .then((cape) => {
+//       try {
+//         resolve(cape
+//           .crop(2, 2, 20, 32)
+//           .scale(scale, Jimp.RESIZE_NEAREST_NEIGHBOR)
+//           .getBufferAsync('image/png'));
+//       }
+//       catch (err) {
+//         resolve(cape
+//           .crop(1, 1, 10, 16)
+//           .scale(scale, Jimp.RESIZE_NEAREST_NEIGHBOR)
+//           .getBufferAsync('image/png'));
+//       }
+//     })
+//     .catch((err) => reject({ error: err, status: 500 }));
+// });
